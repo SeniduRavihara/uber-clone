f57a7c37fd1fdcd01f9b7d149c6faba5538b911e
SeniduRavihara <senidumee16@gmail.com>
2025-04-06 01:22:11 +0530

    map is working

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,55 @@
-import { Counter } from "@/features/counter/Counter";
-import { Text } from "react-native";
+import NavOptions from "@/components/NavOptions";
+import { setDestination, setOrigin } from "@/features/nav/navSlice";
+import { Image, View } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useDispatch } from "react-redux";
 
 export default function HomeScreen() {
+  const dispatch = useDispatch();
+
   return (
-    <SafeAreaView>
-      <Text className="bg-blue-500 text-white">Hello</Text>
-      <Counter />
+    <SafeAreaView className="bg-white w-screen h-screen">
+      <View className="p-5">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
+        />
+
+        <GooglePlacesAutocomplete
+          query={{
+            key: process.env.EXPO_PUBLIC_GOOGLE_MAPS_APIKEY,
+            language: "en",
+          }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details?.geometry.location,
+                description: data.description,
+              })
+            );
+
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          enablePoweredByContainer={false}
+          debounce={400}
+          placeholder="Where From?"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+        />
+
+        <NavOptions />
+      </View>
     </SafeAreaView>
   );
 }
---
+++ components/NavOptions.tsx
@@ -0,0 +1,57 @@
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { Icon } from "@rneui/themed";
+import { useRouter } from "expo-router";
+
+const data = [
+  {
+    id: "1",
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "/",
+  },
+  {
+    id: "2",
+    title: "Order food",
+    image: "https://links.papareact.com/28w",
+    screen: "/map",
+  },
+];
+
+const NavOptions = () => {
+
+  const router = useRouter();
+
+  return (
+    <FlatList
+      data={data}
+      horizontal
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <TouchableOpacity className="p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40" onPress={()=> {
+           router.push(item.screen as any)
+           
+        }}>
+          <View>
+            <Image
+              source={{ uri: item.image }}
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+            />
+
+            <Text className="mt-2 text-lg font-semibold text-center">
+              {item.title}
+            </Text>
+
+            <Icon
+              name="arrowright"
+              color="white"
+              type="antdesign"
+              className="flex items-center justify-center bg-black rounded-full w-10 h-10 mt-4"
+            />
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
+
+export default NavOptions;
